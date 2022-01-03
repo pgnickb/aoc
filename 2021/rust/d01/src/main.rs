bbcb4e402b51a2be17891ca68a6f1132941e85e7
pgnickb <pgnickb@github.com>
2022-01-03 15:13:43 +0100

    Some clean-up
    Added .gitignore
    Reconfigured remote to use ssh-agent (this is a test commit)
    
    WIP: start using clap instead of getopts

--- 2021/rust/d01/src/main.rs
+++ 2021/rust/d01/src/main.rs
@@ -1,3 +1,61 @@
+use std::fs;
+use std::env;
+use std::process;
+use clap::Parser;
+
+#[derive(Parser)]
+#[clap(name    = "AoC 2021 Rust d01")]
+#[clap(author  = "nickb <pgnickb@gmail.com>")]
+#[clap(version = "1.0")]
+#[clap(about   = "Solution to the day 1 of AoC 2021")]
+struct Cli {
+    #[clap(short, long)]
+    /// Enable debug output
+    debug: bool,
+    #[clap(short, long)]
+    /// Print this help and exit
+    help: bool,
+    #[clap(short, long)]
+    /// Print version number and exit
+    version: bool
+    /// Input file
+    file: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+    let mut options = Options::new();
+    options.optflag("h", "help", "show this help, then exit");
+    options.optflag("d", "debug", "output extra debugging information");
+    options.reqopt("f", "", "input file name", "FILE");
+    let cmd_args = match options.parse(&args[1..]) {
+        Err(why) => {
+            println!("Cannot parse command args :{}", why);
+            print_usage_and_exit(&program, &options);
+            return;
+        },
+        Ok(p) => p,
+    };
+
+    if cmd_args.opt_present("h") {
+        print_usage_and_exit(&program, &options);
+    }
+
+
+}
+
+/// Print program usage message and exit
+fn print_usage_and_exit(program: &str, options: &Options){
+    let brief = format!("Usage: {} FILE [options]", program);
+    println!("{}", options.usage(&brief));
+    process::exit(0);
+}
+
+/// Print provided arguments
+fn print_args(cmd_args: getopts::Matches){
+    for opt in cmd_args
+    {
+
+    }
 }
--- 2021/rust/d01/target/rls/debug/deps/d01-95e89635f97a9a05.d
+++
@@ -1,5 +0,0 @@
-/home/nick/my/aoc/2021/rust/d01/target/rls/debug/deps/d01-95e89635f97a9a05.rmeta: src/main.rs
-
-/home/nick/my/aoc/2021/rust/d01/target/rls/debug/deps/d01-95e89635f97a9a05.d: src/main.rs
-
-src/main.rs:
